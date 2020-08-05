a77d2f8d704bb33933fae325d54eb1914476f189
VietAnh Nguyen <vietanhnguyen31@gmail.com>
2020-08-05 09:26:57 +0700

    Feat(webapp): Create database migrations

---
+++ database/migrations/2020_07_31_031637_create_table_orders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('status');
+            $table->tinyInteger('event');
+            $table->timestamp('happened_at');
+            $table->mediumText('comment');
+            $table->integer('customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2020_07_31_041521_create_table_product_comment.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductComment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_comment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('comment_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_comment');
+    }
+}
