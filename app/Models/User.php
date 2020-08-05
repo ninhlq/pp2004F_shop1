06eb5e4ae0b0bdf5563f1d9552d6bdecaed4858b
VietAnh Nguyen <vietanhnguyen31@gmail.com>
2020-08-05 09:46:16 +0700

    Feat(webapp): Create Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getFullName()
+    {
+        return "{$this->last_name} {$this->first_name}";
+    }
 }
--- database/migrations/2020_07_31_032548_create_table_order_log.php
+++ database/migrations/2020_07_31_032548_create_table_order_log.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableBrands extends Migration
+class CreateTableOrderLog extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateTableBrands extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('order_log', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->tinyInteger('event');
+            $table->string('note')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreateTableBrands extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('order_log');
     }
 }
